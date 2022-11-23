dcc9e2d8ce1bdac865140b910e6e277192a25b8a
Edwin <edwin.njoroge@student.moringaschool.com>
2022-11-23 19:50:02 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random () *10) + 1;
+
+let num3 = 53;
+let num4 = 7;
+
+let mod = num3 % num4
+
+let max = Math.max(0,2,5,2,5,19,20)
+
